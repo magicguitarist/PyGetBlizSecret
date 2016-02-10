d4235acb2209594c76e66cf1a26852186ed2bcdb
James Shiell <james@infernus.org>
2016-02-10 20:16:47 +0000

    Updated service hosts for EU/US to new host

--- getkey.py
+++ getkey.py
@@ -15,10 +15,8 @@ RSA_KEY = 257
 
 ENROLL_HOSTS = {
 	"CN": "mobile-service.battlenet.com.cn",
-	"EU": "m.eu.mobileservice.blizzard.com",
-	"US": "m.us.mobileservice.blizzard.com",
-	#"EU": "eu.mobile-service.blizzard.com",
-	#"US": "us.mobile-service.blizzard.com",
+	"EU": "mobile-service.blizzard.com",
+	"US": "mobile-service.blizzard.com",
 	"default": "mobile-service.blizzard.com",
 }
 
